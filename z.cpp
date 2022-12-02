ea9b275fe9ba62eb303e91c7823b6f3f0fcc8bf5
Parvez Ali <urparvezali@gmail.com>
2022-12-02 20:05:49 +0600

    done

--- z.cpp
+++ z.cpp
@@ -15,12 +15,17 @@ int main(){
     {
         for (int j = 0; j < 5-i-1; j++)
         {
-            cout<<" ";
+            cout<<"  ";
         }
         for (int k = 0; k <(i*2)-1;  k++)
         {
-            cout<<"*";
+            cout<<" *";
         }
+        for (int l = 0; l < 5-i-1; l++)
+        {
+            cout<<"  ";
+        }
+        
         cout<<endl;
     }
     
