c8b2577c3753b122dcc2253a6f8f42a6f250572c
Parvez Ali <urparvezali@gmail.com>
2022-12-06 23:01:24 +0600

    done

--- z.cpp
+++ z.cpp
@@ -34,9 +34,17 @@ int main(){
         for (int j = 0; j < 3; j++)
         {
             a[i][j]=a[i][j]+b[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
             cout<<a[i][j]<<" ";
         }
         cout<<endl;
     }
+    
     return 0;
 }
