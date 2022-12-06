28e27dc716523dd972fabb83b1bc3e8f43c521f6
Parvez Ali <urparvezali@gmail.com>
2022-12-06 09:48:36 +0600

    done

--- z.cpp
+++ z.cpp
@@ -9,15 +9,35 @@ typedef vector<ll> vll;
 #define endl "\n"
 #define IOS ios_base::sync_with_stdio(false);cin.tie(NULL)
 
-int inc(int &n){
-    n++;
-    return 0;
-}
 int main(){
     IOS;
-    int n=10;
-    inc(n);
-    cout<<n<<endl;
+    int a[3][3],b[3][3];
+    
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin>>b[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            a[i][j]=a[i][j]+b[i][j];
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     
     return 0;
 }
