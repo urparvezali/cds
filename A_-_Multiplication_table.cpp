7cfc811c8b57cb78d4d0deaada5f27d54e5f0b94
Parvez Ali <urparvezali@gmail.com>
2022-12-08 22:13:32 +0600

    done

--- A_-_Multiplication_table.cpp
+++ A_-_Multiplication_table.cpp
@@ -10,6 +10,12 @@ typedef vector<ll> vll;
 #define mod (1e9+7)
 
 int main(){
+    ll n;
+    cin>>n;
+    for (int i = 1; i <= 12; i++)
+    {
+        cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+    }
     
     return 0;
 }
