cc3631df47d1fa4a1e7d62b358c46d190a1402ae
Parvez Ali <urparvezali@gmail.com>
2022-12-19 17:45:17 +0600

    done

--- z.cpp
+++ z.cpp
@@ -1,20 +1,10 @@
-// Parvez Ali, ICE-13 of PUST
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-typedef vector<int> vint;
-typedef vector<ll> vll;
-typedef vector<char> vchar;
-typedef vector<string> vstr;
-#define endl '\n'
-#define mod (1e9+7)
-void Y(){cout<<"YES"<<endl;}
-void N(){cout<<"NO"<<endl;}
-void IOS(){ios_base::sync_with_stdio(false); cin.tie(NULL);}
-
 int main(){
-    IOS();
-    
-    return 0;
+long long a;
+cin>>a;
+long long arr[a];
+for(int &i:arr)cin>>i;
+for(int &j:arr)cout<<j<<" ";
+return 0;
 }
